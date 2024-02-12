e2fa6eb9693d0643247c4ddef0f24b1f9225dea7
Albin Horlaville <albin.horlaville@etu.univ-grenoble-alpes.fr>
2024-02-12 20:16:33 +0100

    MapProcedural with perlin noise

--- OpenWorldLego/levels/main.gd
+++ OpenWorldLego/levels/main.gd
@@ -2,12 +2,24 @@ extends Node
 
 @export var dirt_scene: PackedScene
 @export var limit_map: Vector3
+@export var perlin_noise: Noise
+@export var color_dirt: Material
+@export var color_water: Material
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	perlin_noise.seed = randi()
 	for i in range(limit_map.x):
 		for j in range(limit_map.z):
 			var dirt = dirt_scene.instantiate()
+			generateGround(dirt, i, j)
 			dirt.position = Vector3(i, 0, j)
 			add_child(dirt)
-	print(self.get_path())
+	
+func generateGround(block:Node, x, y) -> void:
+	var currentPN = perlin_noise.get_noise_2d(x, y)
+	if currentPN > -0.1:
+		block.changeMaterial(color_dirt)
+	else:
+		block.changeMaterial(color_water)
+	

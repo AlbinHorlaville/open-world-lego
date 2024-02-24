fcb3c7102aa00a84ce40c96020596f3202e6d765
Albin Horlaville <albin.horlaville@etu.univ-grenoble-alpes.fr>
2024-02-24 19:18:48 +0100

    Optimisation

--- OpenWorldLego/levels/main.gd
+++ OpenWorldLego/levels/main.gd
@@ -7,6 +7,8 @@ extends Node
 var DictChunk_ON:Dictionary # Dictionnary of chunks charged
 var DictChunk_OFF:Dictionary # Dictionnary of chunks uncharged
 var ChunksToBuild:Array # Chunks that need to be generate in the few next frames
+var count:int # Genere 1 chunk per frame
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	DictChunk_ON = {}
@@ -19,7 +21,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	# Create 1 chunk as maximum per frame
 	count+=1
-	if count==3:
+	if count==4:
 		ChunkInit()
 		count=0
 	
@@ -33,7 +35,6 @@ func _process(_delta: float) -> void:
 			DictChunk_OFF[pos_chunk] = chunk
 			chunk.set_visible(false)
 			DictChunk_ON.erase(pos_chunk)
-			$World.remove_child(chunk)
 
 	# Update DictChunk_ON and DictChunk_OFF
 	for pos_chunk in neighbors:
@@ -45,7 +46,6 @@ func _process(_delta: float) -> void:
 			DictChunk_OFF.erase(pos_chunk)
 			DictChunk_ON[pos_chunk] = chunk
 			chunk.set_visible(true)
-			$World.add_child(chunk)
 			continue
 		if not pos_chunk in ChunksToBuild:
 			# Case of new chunk who as to be charged
@@ -53,13 +53,11 @@ func _process(_delta: float) -> void:
 			if limit_map.x/16>=pos_chunk.x and pos_chunk.x>=0 and limit_map.z/16>=pos_chunk.y and pos_chunk.y>=0:
 				ChunksToBuild.append(pos_chunk)
 
-var count:int
 func ChunkInit():
 	if len(ChunksToBuild)>0:
 		var pos_chunk = ChunksToBuild.pop_front()
 		var chunk = Chunk.instantiate()
 		chunk.CreateChunk(pos_chunk.x, pos_chunk.y)
-		chunk.set_visible(true)
 		DictChunk_ON[pos_chunk] = chunk
 		$World.add_child(chunk)
 		return

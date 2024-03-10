a7ccf5be88a0f56c2e269929735904ee0d5a2fc0
Grégory Pititto <gregory.pititto@etu.univ-grenoble-alpes.fr>
2024-03-10 22:39:13 +0100

    v1 de la vue FPS du player

--- OpenWorldLego/actor/player.gd
+++ OpenWorldLego/actor/player.gd
@@ -1,18 +1,53 @@
 extends CharacterBody3D
 
-
+# variable keyboard
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
+
+#variable mouse
+
+# var pour bloquer la caméra (ne pas pouvoir regarder en arrière)
+var lookAngle = 90.0
+var mouseSensitivity = 0.4
+# mouvement de la souris
+var mouseDelta : Vector2
+
+
+var camera : Camera3D 
+
 # limit of the viewing range of the player (in chunks)
 var limit_view:int = 2
 
 
+func _ready():
+	# On récup le neoud camera
+	camera = get_node("Camera3D")
+	
+	
+func _input(event):
+	if event is InputEventMouseMotion :
+		# On récup le mouvement de la souris
+		mouseDelta = event.relative
+
+
+func _process(delta):
+	# On applique la rotation sur la caméra (axe y)
+	camera.rotation_degrees -= Vector3(rad_to_deg(mouseDelta.y),0,0) * mouseSensitivity * delta
+	
+	#camera.rotation_degrees +=  Vector3(clamp(camera.rotation_degrees.x,-lookAngle,lookAngle),camera.rotation_degrees.y, camera.rotation_degrees.z)
+	
+	# rotation sur le perso
+	rotation_degrees -= Vector3(0,rad_to_deg(mouseDelta.x),0) * mouseSensitivity * delta
+	# reinit 
+	mouseDelta = Vector2()
+	
+
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	# Add the gravity
 	if not is_on_floor():
 		velocity.y -= gravity * delta
 

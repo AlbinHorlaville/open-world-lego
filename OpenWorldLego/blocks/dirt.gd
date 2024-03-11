09d11a6bdd7f0be4b6135bd1dd68a69bea3515d4
RomainMIRAS <73794188+RomainMIRAS@users.noreply.github.com>
2024-03-11 16:13:28 +0100

    Fix Par Ajout nouvelle piéce

--- OpenWorldLego/blocks/dirt.gd
+++ OpenWorldLego/blocks/dirt.gd
@@ -2,6 +2,6 @@ extends StaticBody3D
 
 func changeMaterial(mat:Material)->void:
 	# get the instance
-	var mesh_inst = $brick_2x2.get_child(0)
+	var mesh_inst = $brick2x2.get_child(0)
 	# Assign the material
 	mesh_inst.set_surface_override_material(0, mat)

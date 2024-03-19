84166adb2d0382bc00b01c35a352a9d93443583f
LouaneL <lou.lesur@orange.fr>
2024-03-19 15:14:28 +0100

    finish

--- OpenWorldLego/blocks/cloud.gd
+++ OpenWorldLego/blocks/cloud.gd
@@ -1,10 +1,23 @@
 extends Node3D
+@export var model1: PackedScene
+@export var model2: PackedScene
+@export var model3: PackedScene
 
 var Player
 var vitesse
 var distance
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var n = randi_range(0, 2)
+	if n==0:
+		var model = model1.instantiate()
+		add_child(model)
+	elif n==1:
+		var model = model2.instantiate()
+		add_child(model)
+	else:
+		var model = model3.instantiate()
+		add_child(model)
 	pass # Replace with function body.
 
 
@@ -25,5 +38,6 @@ func _process(delta):
 	if position.z < posz_player - distance:
 		position.z = posz_player + distance
 	
-	position.x += vitesse*delta
+	position.x += vitesse*delta*0.1
 	pass
+

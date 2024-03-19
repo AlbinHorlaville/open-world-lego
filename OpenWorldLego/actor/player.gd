6198d619f446cd81252362fedb648c4f7fcd9a58
Grégory Pititto <gregory.pititto@etu.univ-grenoble-alpes.fr>
2024-03-19 19:50:10 +0100

    Changement de vue de 1ère a 3ème pers et inv

--- OpenWorldLego/actor/player.gd
+++ OpenWorldLego/actor/player.gd
@@ -46,8 +46,8 @@ func _process(delta):
 	# Passage de la vue 1ère personne à la vue 3ème personne
 	if Input.is_action_just_pressed("toggle_view") and isFirstPerson:
 		isFirstPerson = false
-		camera.position.y = 3.897
-		camera.position.z = 5.607
+		camera.position.y = 1.700
+		camera.position.z = 1.700
 	elif Input.is_action_just_pressed("toggle_view") and !isFirstPerson:
 		isFirstPerson = true
 		camera.position.y = 0.915

898ecd6799d903cb3a6692a56ff937b959ac16d2
LouaneL <lou.lesur@orange.fr>
2024-04-02 14:04:46 +0200

    Refactoring et ajout commentaires Cloud et Tree

--- OpenWorldLego/blocks/tree.gd
+++ OpenWorldLego/blocks/tree.gd
@@ -14,16 +14,61 @@ extends Node3D
 # Dimension Lego piece 2x2 = 16x16x9,6
 var hlego:float = 9.6/16
 
-# Readjust the color to paint a tree
-func recAdjutsColor(node, material):
-	for n in node.get_children():
-		if n is Brick:
-			n.changeMaterial(material)
-			# Readjust the y position because a lego bric is not 1 by 1 by 1
-			n.position.y = n.position.y * hlego
-		else:
-			recAdjutsColor(n, material)
+# Called when the node enters the scene tree for the first time.
+# When a cloud is initialized, a skin is chosen
+func _ready() -> void:
+	var int_trunk = randi_range(1,3)
+	var int_foliage = randi_range(1,4)
+	
+	var trunk = generateTrunk(int_trunk)
+	var h_trunk = heightTrunk(int_trunk)
+	addCollision(trunk)
+	changeColor(trunk,2)
+	add_child(trunk)
+	
+	var foliage = generateFoliage(int_foliage)
+	foliage.position.y = (h_trunk)*hlego/62.5 + trunk.position.y
+	addCollision(foliage)
+	changeColor(foliage,1)
+	add_child(foliage)
+	
+# A trunk skin is chosen
+func generateTrunk(int_trunk):
+	var trunk
+	if int_trunk == 1:
+		trunk = trunk1.instantiate()
+	elif int_trunk == 2:
+		trunk = trunk2.instantiate()
+	else:
+		trunk = trunk3.instantiate()
+	return trunk
 
+func heightTrunk(int_trunk):
+	var htrunk
+	if int_trunk == 1:
+		htrunk = 5
+	elif int_trunk == 2:
+		htrunk = 6
+	else:
+		htrunk = 7
+	
+	return htrunk
+
+# A foliage skin is chosen
+func generateFoliage(int_foliage):
+	var foliage
+	if int_foliage == 1:
+		foliage = foliage1.instantiate()
+	elif int_foliage == 2:
+		foliage = foliage2.instantiate()
+	elif int_foliage == 3:
+		foliage = foliage3.instantiate()
+	else:
+		foliage = foliage4.instantiate()
+	
+	return foliage
+
+# The color is changed, mode=1 for foliage and mode=2 for trunk
 func changeColor(node,mode):
 	if mode == 1:
 		for child in node.get_children():
@@ -35,6 +80,8 @@ func changeColor(node,mode):
 				child.set_surface_override_material(0, color_wood)
 		
 		
+
+# Collision is added for each children of the node
 func addCollision (node):
 	var collision
 	var box
@@ -42,39 +89,3 @@ func addCollision (node):
 	for child in node.get_children():
 		if not(child is AnimationPlayer):
 			child.create_trimesh_collision()
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	var int_trunk = randi_range(1,3)
-	var int_foliage = randi_range(1,4)
-	
-	var h_trunk
-	var trunk
-	if int_trunk == 1:
-		trunk = trunk1.instantiate()
-		h_trunk = 5
-	elif int_trunk == 2:
-		trunk = trunk2.instantiate()
-		h_trunk = 6
-	else:
-		trunk = trunk3.instantiate()
-		h_trunk = 7
-	addCollision(trunk)
-	changeColor(trunk,2)
-	add_child(trunk)
-	
-	var foliage
-	if int_foliage == 1:
-		foliage = foliage1.instantiate()
-	elif int_foliage == 2:
-		foliage = foliage2.instantiate()
-	elif int_foliage == 3:
-		foliage = foliage3.instantiate()
-	else:
-		foliage = foliage4.instantiate()
-		
-	foliage.position.y = (h_trunk)*hlego/62.5 + trunk.position.y
-	addCollision(foliage)
-	changeColor(foliage,1)
-	add_child(foliage)
-	
